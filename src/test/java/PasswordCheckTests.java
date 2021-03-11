b22f5227979c9f716b4d407454905fcb7f10f607
GabrielHuebner <71716193+GabrielHuebner@users.noreply.github.com>
2021-03-11 19:02:09 +0100

    deleted comments

--- src/test/java/PasswordCheckTests.java
+++ src/test/java/PasswordCheckTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Link to repository: https://github.com/GabrielHuebner/prog2-ss21-exercise1.git
+
 public class PasswordCheckTests {
     private PasswordCheck pw = new PasswordCheck();
 
@@ -17,7 +19,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if a valid password is accepted")
     @Test
     void checkPasswordTrue() {
-        //check if PW too short
         boolean b = pw.checkPassword("Aa1!aaw2j");
         assertTrue(b);
     }
@@ -25,7 +26,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if too short password is accepted")
     @Test
     void checkPasswordLength1() {
-        //check if PW too short
         boolean b = pw.checkPassword("AAA1!a");
         assertFalse(b);
     }
@@ -33,7 +33,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if too long password is accepted")
     @Test
     void checkPasswordLength2() {
-        //check if PW too long
         boolean b = pw.checkPassword("AAAAAAAAAAAAAAAAAAAAA1!aAAAAAA");
         assertFalse(b);
     }
@@ -41,7 +40,6 @@ public class PasswordCheckTests {
     @DisplayName("Check all lowercase password is accepted")
     @Test
     void checkPasswordUpperCase() {
-        //check if it checks for Uppercase
         boolean b = pw.checkPassword("aaa1!aaa");
         assertFalse(b);
     }
@@ -57,7 +55,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if password without digits is accepted")
     @Test
     void checkPasswordDigit() {
-        //check if it checks Numbers
         boolean b = pw.checkPassword("AAAa!AAA");
         assertFalse(b);
     }
@@ -65,7 +62,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if password without special sign is accepted")
     @Test
     void checkPasswordSpecialChar() {
-        //check if it checks for Special Characters
         boolean b = pw.checkPassword("AAAa1AAA");
         assertFalse(b);
     }
@@ -73,7 +69,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if password with 4 repeating numbers is accepted")
     @Test
     void checkPasswordRepeatingNumbers() {
-        //check if it checks for RepeatingNumbers eg. 1111
         boolean b = pw.checkPassword("1111AAAa1AA!");
         assertFalse(b);
     }
@@ -81,7 +76,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if password with 3 rising numbers is accepted")
     @Test
     void checkPasswordRisingNumbers() {
-        //check if it checks for Rising Numbers eg. 123
         boolean b = pw.checkPassword("123AAAa1AAA");
         assertFalse(b);
     }
@@ -89,7 +83,6 @@ public class PasswordCheckTests {
     @DisplayName("Check null value is accepted")
     @Test
     void checkPasswordNullValue() {
-        //check if it checks for Rising Numbers eg. 123
         boolean b = pw.checkPassword(null);
         assertFalse(b);
     }
@@ -97,7 +90,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if password with space is accepted")
     @Test
     void checkPasswordOtherCharacter1() {
-        //check if it checks for Rising Numbers eg. 123
         boolean b = pw.checkPassword("Aa1! aaw2j");
         assertFalse(b);
     }
@@ -105,7 +97,6 @@ public class PasswordCheckTests {
     @DisplayName("Check if password with invalid character is accepted")
     @Test
     void checkPasswordOtherCharacter2() {
-        //check if it checks for Rising Numbers eg. 123
         boolean b = pw.checkPassword("Aa1!{aaw2j");
         assertFalse(b);
     }
